f98e5765b0b97a7c9d308c988d29236532493135
Ilya Senatorov <il.senatorov@protonmail.com>
2023-07-23 11:40:00 +0200

    first

---
+++ valorant.py
@@ -0,0 +1,27 @@
+agent_roles = {
+    "brimstone": "controller",
+    "viper": "controller",
+    "omen": "controller",
+    "killjoy": "sentinel",
+    "cypher": "sentinel",
+    "sova": "initiator",
+    "sage": "sentinel",
+    "phoenix": "duelist",
+    "jett": "duelist",
+    "reyna": "duelist",
+    "raze": "duelist",
+    "breach": "initiator",
+    "skye": "initiator",
+    "yoru": "duelist",
+    "astra": "controller",
+    "kay/o": "initiator",
+    "chamber": "sentinel",
+    "neon": "duelist",
+    "fade": "initiator",
+    "harbor": "controller",
+    "gekko": "initiator",
+    "deadlock": "sentinel",
+}
+emea = ["FNC", "NAVI", "BBL", "FUT", "TL", "TH", "KOI", "KC", "GIA", "VIT"]
+amer = ["LOUD", "NRG", "EG", "C9", "LEV", "FUR", "MIBR", "KRÜ", "SEN", "100T"]
+apac = ["T1", "PRX", "RRQ", "GE", "DRX", "DFM", "GENG", "ZETA", "TLN", "TS"]
